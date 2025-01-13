2fc1a63658f31ded529329513d287618bb9927d2
laurent <email.laurent@gmail.com>
2025-01-13 23:04:06 +0100

    menu home v0.1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,8 +2,24 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonItem,
+  IonList,
+  IonRow,
+  IonThumbnail,
+} from '@ionic/angular/standalone';
 import { TranslateModule } from '@ngx-translate/core';
 import { LayoutComponent } from '../layout/layout.component';
+import { FilmsResponse, UsersResponse } from './home';
 import { HomeService } from './home.service';
 
 @Component({
@@ -11,10 +27,30 @@ import { HomeService } from './home.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, TranslateModule, LayoutComponent],
+  imports: [
+    IonRow,
+    IonCol,
+    IonTitle,
+    IonGrid,
+    IonThumbnail,
+    IonItem,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonList,
+    IonContent,
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    LayoutComponent,
+  ],
 })
 export class HomePage implements OnInit {
   protected message = 'angular content';
+  protected users: UsersResponse[] = [];
+  protected films: FilmsResponse[] = [];
 
   constructor(private http: HttpClient, private homeService: HomeService) {}
 
@@ -22,6 +58,15 @@ export class HomePage implements OnInit {
     this.homeService.getHello().subscribe({
       next: (data) => {
         this.message = data.message;
+      },
+      error: (error) => {
+        console.error('An error occurred:', error);
+      },
+    });
+
+    this.homeService.getFilms().subscribe({
+      next: (data: FilmsResponse[]) => {
+        this.films = data;
       },
       error: (error) => {
         console.error('An error occurred:', error);
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,3 +1,26 @@
-export interface HomeResponse {
+export interface HelloResponse {
   message: string;
+}
+
+export interface UsersResponse {
+  id: number;
+  login: string;
+  password: string;
+  reset_password: boolean;
+  first_name: string;
+  last_name: string;
+  email: string;
+  username: string;
+  role_id: number;
+  created_at: string;
+}
+
+export interface FilmsResponse {
+  id: number;
+  title: string;
+  description: string;
+  release_date: Date;
+  age_minimum: number;
+  favorite: number;
+  poster: string;
 }
